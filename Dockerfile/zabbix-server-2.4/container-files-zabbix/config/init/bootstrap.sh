a65bf2ec1a57a22fc3aa97a59f893ec4ea86431a
janga <jan.garaj@multiplay.co.uk>
2015-08-11 16:29:05 +0100

    Fix

--- Dockerfile/zabbix-server-2.4/container-files-zabbix/config/init/bootstrap.sh
+++ Dockerfile/zabbix-server-2.4/container-files-zabbix/config/init/bootstrap.sh
@@ -21,14 +21,14 @@ error() {
   echo "${bold}${red}[ERROR `date +'%T'`]${reset} ${red}$@${reset}";
 }
 create_db() {
-  mysql -u ${DB_USER} -p${DB_PASS} -h ${DB_ADDR} -e "CREATE DATABASE IF NOT EXISTS zabbix CHARACTER SET utf8;"
-  mysql -u ${DB_USER} -p${DB_PASS} -h ${DB_ADDR} -e "GRANT ALL ON zabbix.* TO '${DB_USER}'@'%' identified by '${DB_PASS}';"
-  mysql -u ${DB_USER} -p${DB_PASS} -h ${DB_ADDR} -e "flush privileges;"
+  mysql -u ${ZS_DBUser} -p${ZS_DBPassword} -h ${ZS_DBHost} -P ${ZS_DBPort} -e "CREATE DATABASE IF NOT EXISTS ${ZS_DBName} CHARACTER SET utf8;"
+  mysql -u ${ZS_DBUser} -p${ZS_DBPassword} -h ${ZS_DBHost} -P ${ZS_DBPort} -e "GRANT ALL ON ${ZS_DBName}.* TO '${ZS_DBUser}'@'%' identified by '${ZS_DBPassword}';"
+  mysql -u ${ZS_DBUser} -p${ZS_DBPassword} -h ${ZS_DBHost} -P ${ZS_DBPort} -e "flush privileges;"
 }
 import_zabbix_db() {
-  mysql -u ${DB_USER} -p${DB_PASS} -h ${DB_ADDR} -D zabbix < ${ZABBIX_SQL_DIR}/schema.sql
-  mysql -u ${DB_USER} -p${DB_PASS} -h ${DB_ADDR} -D zabbix < ${ZABBIX_SQL_DIR}/images.sql
-  mysql -u ${DB_USER} -p${DB_PASS} -h ${DB_ADDR} -D zabbix < ${ZABBIX_SQL_DIR}/data.sql
+  mysql -u ${ZS_DBUser} -p${ZS_DBPassword} -h ${ZS_DBHost} -P ${ZS_DBPort} -D ${ZS_DBName} < ${ZABBIX_SQL_DIR}/schema.sql
+  mysql -u ${ZS_DBUser} -p${ZS_DBPassword} -h ${ZS_DBHost} -P ${ZS_DBPort} -D ${ZS_DBName} < ${ZABBIX_SQL_DIR}/images.sql
+  mysql -u ${ZS_DBUser} -p${ZS_DBPassword} -h ${ZS_DBHost} -P ${ZS_DBPort} -D ${ZS_DBName} < ${ZABBIX_SQL_DIR}/data.sql
 }
 logging() {
   mkdir -p /var/log/zabbix
@@ -50,69 +50,76 @@ fix_permissions() {
   chmod u+s `which ping`
 }
 update_config() {
-  sed -i 's/=ZS_ListenPort/='${ZS_ListenPort}'/g' /usr/local/etc/zabbix_server.conf
-  sed -i 's/=ZS_SourceIP/='${ZS_SourceIP}'/g' /usr/local/etc/zabbix_server.conf
-  sed -i 's/=ZS_LogFile/='${ZS_LogFile}'/g' /usr/local/etc/zabbix_server.conf
-  sed -i 's/=ZS_LogFileSize/='${ZS_LogFileSize}'/g' /usr/local/etc/zabbix_server.conf
-  sed -i 's/=ZS_DebugLevel/='${ZS_DebugLevel}'/g' /usr/local/etc/zabbix_server.conf
-  sed -i 's/=ZS_PidFile/='${ZS_PidFile}'/g' /usr/local/etc/zabbix_server.conf
-  sed -i 's/=ZS_DBHost/='${ZS_DBHost}'/g' /usr/local/etc/zabbix_server.conf
-  sed -i 's/=ZS_DBName/='${ZS_DBName}'/g' /usr/local/etc/zabbix_server.conf
-  sed -i 's/=ZS_DBSchema/='${ZS_DBSchema}'/g' /usr/local/etc/zabbix_server.conf
-  sed -i 's/=ZS_DBUser/='${ZS_DBUser}'/g' /usr/local/etc/zabbix_server.conf
-  sed -i 's/=ZS_DBPassword/='${ZS_DBPassword}'/g' /usr/local/etc/zabbix_server.conf
-  sed -i 's/=ZS_DBSocket/='${ZS_DBSocket}'/g' /usr/local/etc/zabbix_server.conf
-  sed -i 's/=ZS_DBPort/='${ZS_DBPort}'/g' /usr/local/etc/zabbix_server.conf
-  sed -i 's/=ZS_StartPollers/='${ZS_StartPollers}'/g' /usr/local/etc/zabbix_server.conf
-  sed -i 's/=ZS_StartPollersUnreachable/='${ZS_StartPollersUnreachable}'/g' /usr/local/etc/zabbix_server.conf
-  sed -i 's/=ZS_StartTrappers/='${ZS_StartTrappers}'/g' /usr/local/etc/zabbix_server.conf
-  sed -i 's/=ZS_StartPingers/='${ZS_StartPingers}'/g' /usr/local/etc/zabbix_server.conf
-  sed -i 's/=ZS_StartDiscoverers/='${ZS_StartDiscoverers}'/g' /usr/local/etc/zabbix_server.conf
-  sed -i 's/=ZS_StartHTTPPollers/='${ZS_StartHTTPPollers}'/g' /usr/local/etc/zabbix_server.conf
-  sed -i 's/=ZS_StartTimers/='${ZS_StartTimers}'/g' /usr/local/etc/zabbix_server.conf
-  sed -i 's/=ZS_JavaGateway/='${ZS_JavaGateway}'/g' /usr/local/etc/zabbix_server.conf
-  sed -i 's/=ZS_JavaGatewayPort/='${ZS_JavaGatewayPort}'/g' /usr/local/etc/zabbix_server.conf
-  sed -i 's/=ZS_StartJavaPollers/='${ZS_StartJavaPollers}'/g' /usr/local/etc/zabbix_server.conf
-  sed -i 's/=ZS_StartVMwareCollectors/='${ZS_StartVMwareCollectors}'/g' /usr/local/etc/zabbix_server.conf
-  sed -i 's/=ZS_VMwareFrequency/='${ZS_VMwareFrequency}'/g' /usr/local/etc/zabbix_server.conf
-  sed -i 's/=ZS_VMwarePerfFrequency/='${ZS_VMwarePerfFrequency}'/g' /usr/local/etc/zabbix_server.conf
-  sed -i 's/=ZS_VMwareCacheSize/='${ZS_VMwareCacheSize}'/g' /usr/local/etc/zabbix_server.conf
-  sed -i 's/=ZS_VMwareTimeout/='${ZS_VMwareTimeout}'/g' /usr/local/etc/zabbix_server.conf
-  sed -i 's/=ZS_SNMPTrapperFile/='${ZS_SNMPTrapperFile}'/g' /usr/local/etc/zabbix_server.conf
-  sed -i 's/=ZS_StartSNMPTrapper/='${ZS_StartSNMPTrapper}'/g' /usr/local/etc/zabbix_server.conf
-  sed -i 's/=ZS_ListenIP/='${ZS_ListenIP}'/g' /usr/local/etc/zabbix_server.conf
-  sed -i 's/=ZS_HousekeepingFrequency/='${ZS_HousekeepingFrequency}'/g' /usr/local/etc/zabbix_server.conf
-  sed -i 's/=ZS_MaxHousekeeperDelete/='${ZS_MaxHousekeeperDelete}'/g' /usr/local/etc/zabbix_server.conf
-  sed -i 's/=ZS_SenderFrequency/='${ZS_SenderFrequency}'/g' /usr/local/etc/zabbix_server.conf
-  sed -i 's/=ZS_CacheSize/='${ZS_CacheSize}'/g' /usr/local/etc/zabbix_server.conf
-  sed -i 's/=ZS_CacheUpdateFrequency/='${ZS_CacheUpdateFrequency}'/g' /usr/local/etc/zabbix_server.conf
-  sed -i 's/=ZS_StartDBSyncers/='${ZS_StartDBSyncers}'/g' /usr/local/etc/zabbix_server.conf
-  sed -i 's/=ZS_HistoryCacheSize/='${ZS_HistoryCacheSize}'/g' /usr/local/etc/zabbix_server.conf
-  sed -i 's/=ZS_ValueCacheSize/='${ZS_ValueCacheSize}'/g' /usr/local/etc/zabbix_server.conf
-  sed -i 's/=ZS_Timeout/='${ZS_Timeout}'/g' /usr/local/etc/zabbix_server.conf
-  sed -i 's/=ZS_TrapperTimeout/='${ZS_TrapperTimeout}'/g' /usr/local/etc/zabbix_server.conf
-  sed -i 's/=ZS_UnreachablePeriod/='${ZS_UnreachablePeriod}'/g' /usr/local/etc/zabbix_server.conf
-  sed -i 's/=ZS_UnavailableDelay/='${ZS_UnavailableDelay}'/g' /usr/local/etc/zabbix_server.conf
-  sed -i 's/=ZS_UnreachableDelay/='${ZS_UnreachableDelay}'/g' /usr/local/etc/zabbix_server.conf
-  sed -i 's/=ZS_AlertScriptsPath/='${ZS_AlertScriptsPath}'/g' /usr/local/etc/zabbix_server.conf
-  sed -i 's/=ZS_ExternalScripts/='${ZS_ExternalScripts}'/g' /usr/local/etc/zabbix_server.conf
-  sed -i 's/=ZS_FpingLocation/='${ZS_FpingLocation}'/g' /usr/local/etc/zabbix_server.conf
-  sed -i 's/=ZS_Fping6Location/='${ZS_Fping6Location}'/g' /usr/local/etc/zabbix_server.conf
-  sed -i 's/=ZS_SSHKeyLocation/='${ZS_SSHKeyLocation}'/g' /usr/local/etc/zabbix_server.conf
-  sed -i 's/=ZS_LogSlowQueries/='${ZS_LogSlowQueries}'/g' /usr/local/etc/zabbix_server.conf
-  sed -i 's/=ZS_TmpDir/='${ZS_TmpDir}'/g' /usr/local/etc/zabbix_server.conf
-  sed -i 's/=ZS_StartProxyPollers/='${ZS_StartProxyPollers}'/g' /usr/local/etc/zabbix_server.conf
-  sed -i 's/=ZS_ProxyConfigFrequency/='${ZS_ProxyConfigFrequency}'/g' /usr/local/etc/zabbix_server.conf
-  sed -i 's/=ZS_ProxyDataFrequency/='${ZS_ProxyDataFrequency}'/g' /usr/local/etc/zabbix_server.conf
-  sed -i 's/=ZS_AllowRoot/='${ZS_AllowRoot}'/g' /usr/local/etc/zabbix_server.conf
-  sed -i 's/=ZS_User/='${ZS_User}'/g' /usr/local/etc/zabbix_server.conf
-  sed -i 's/=ZS_Include/='${ZS_Include}'/g' /usr/local/etc/zabbix_server.conf
-  sed -i 's/=ZS_SSLCertLocation/='${ZS_SSLCertLocation}'/g' /usr/local/etc/zabbix_server.conf
-  sed -i 's/=ZS_SSLKeyLocation/='${ZS_SSLKeyLocation}'/g' /usr/local/etc/zabbix_server.conf
-  sed -i 's/=ZS_SSLCALocation/='${ZS_SSLCALocation}'/g' /usr/local/etc/zabbix_server.conf
-  sed -i 's/=ZS_LoadModulePath/='${ZS_LoadModulePath}'/g' /usr/local/etc/zabbix_server.conf
-  sed -i 's/=ZS_LoadModule/='${ZS_LoadModule}'/g' /usr/local/etc/zabbix_server.conf
-
+  sed -i 's#=ZS_ListenPort#='${ZS_ListenPort}'#g' /usr/local/etc/zabbix_server.conf
+  if [ "$ZS_SourceIP" != "" ]; then
+    echo SourceIP=${ZS_SourceIP} >> /usr/local/etc/zabbix_server.conf
+  fi
+  sed -i 's#=ZS_LogFileSize#='${ZS_LogFileSize}'#g' /usr/local/etc/zabbix_server.conf
+  sed -i 's#=ZS_LogFile#='${ZS_LogFile}'#g' /usr/local/etc/zabbix_server.conf
+  sed -i 's#=ZS_DebugLevel#='${ZS_DebugLevel}'#g' /usr/local/etc/zabbix_server.conf
+  sed -i 's#=ZS_PidFile#='${ZS_PidFile}'#g' /usr/local/etc/zabbix_server.conf
+  sed -i 's#=ZS_DBHost#='${ZS_DBHost}'#g' /usr/local/etc/zabbix_server.conf
+  sed -i 's#=ZS_DBName#='${ZS_DBName}'#g' /usr/local/etc/zabbix_server.conf
+  sed -i 's#=ZS_DBSchema#='${ZS_DBSchema}'#g' /usr/local/etc/zabbix_server.conf
+  sed -i 's#=ZS_DBUser#='${ZS_DBUser}'#g' /usr/local/etc/zabbix_server.conf
+  sed -i 's#=ZS_DBPassword#='${ZS_DBPassword}'#g' /usr/local/etc/zabbix_server.conf
+  sed -i 's#=ZS_DBSocket#='${ZS_DBSocket}'#g' /usr/local/etc/zabbix_server.conf
+  sed -i 's#=ZS_DBPort#='${ZS_DBPort}'#g' /usr/local/etc/zabbix_server.conf
+  sed -i 's#=ZS_StartPollersUnreachable#='${ZS_StartPollersUnreachable}'#g' /usr/local/etc/zabbix_server.conf
+  sed -i 's#=ZS_StartPollers#='${ZS_StartPollers}'#g' /usr/local/etc/zabbix_server.conf
+  sed -i 's#=ZS_StartTrappers#='${ZS_StartTrappers}'#g' /usr/local/etc/zabbix_server.conf
+  sed -i 's#=ZS_StartPingers#='${ZS_StartPingers}'#g' /usr/local/etc/zabbix_server.conf
+  sed -i 's#=ZS_StartDiscoverers#='${ZS_StartDiscoverers}'#g' /usr/local/etc/zabbix_server.conf
+  sed -i 's#=ZS_StartHTTPPollers#='${ZS_StartHTTPPollers}'#g' /usr/local/etc/zabbix_server.conf
+  sed -i 's#=ZS_StartTimers#='${ZS_StartTimers}'#g' /usr/local/etc/zabbix_server.conf
+  sed -i 's#=ZS_JavaGatewayPort#='${ZS_JavaGatewayPort}'#g' /usr/local/etc/zabbix_server.conf
+  sed -i 's#=ZS_JavaGateway#='${ZS_JavaGateway}'#g' /usr/local/etc/zabbix_server.conf
+  sed -i 's#=ZS_StartJavaPollers#='${ZS_StartJavaPollers}'#g' /usr/local/etc/zabbix_server.conf
+  sed -i 's#=ZS_StartVMwareCollectors#='${ZS_StartVMwareCollectors}'#g' /usr/local/etc/zabbix_server.conf
+  sed -i 's#=ZS_VMwareFrequency#='${ZS_VMwareFrequency}'#g' /usr/local/etc/zabbix_server.conf
+  sed -i 's#=ZS_VMwarePerfFrequency#='${ZS_VMwarePerfFrequency}'#g' /usr/local/etc/zabbix_server.conf
+  sed -i 's#=ZS_VMwareCacheSize#='${ZS_VMwareCacheSize}'#g' /usr/local/etc/zabbix_server.conf
+  sed -i 's#=ZS_VMwareTimeout#='${ZS_VMwareTimeout}'#g' /usr/local/etc/zabbix_server.conf
+  sed -i 's#=ZS_SNMPTrapperFile#='${ZS_SNMPTrapperFile}'#g' /usr/local/etc/zabbix_server.conf
+  sed -i 's#=ZS_StartSNMPTrapper#='${ZS_StartSNMPTrapper}'#g' /usr/local/etc/zabbix_server.conf
+  sed -i 's#=ZS_ListenIP#='${ZS_ListenIP}'#g' /usr/local/etc/zabbix_server.conf
+  sed -i 's#=ZS_HousekeepingFrequency#='${ZS_HousekeepingFrequency}'#g' /usr/local/etc/zabbix_server.conf
+  sed -i 's#=ZS_MaxHousekeeperDelete#='${ZS_MaxHousekeeperDelete}'#g' /usr/local/etc/zabbix_server.conf
+  sed -i 's#=ZS_SenderFrequency#='${ZS_SenderFrequency}'#g' /usr/local/etc/zabbix_server.conf
+  sed -i 's#=ZS_CacheSize#='${ZS_CacheSize}'#g' /usr/local/etc/zabbix_server.conf
+  sed -i 's#=ZS_CacheUpdateFrequency#='${ZS_CacheUpdateFrequency}'#g' /usr/local/etc/zabbix_server.conf
+  sed -i 's#=ZS_StartDBSyncers#='${ZS_StartDBSyncers}'#g' /usr/local/etc/zabbix_server.conf
+  sed -i 's#=ZS_HistoryCacheSize#='${ZS_HistoryCacheSize}'#g' /usr/local/etc/zabbix_server.conf
+  sed -i 's#=ZS_TrendCacheSize#='${ZS_TrendCacheSize}'#g' /usr/local/etc/zabbix_server.conf
+  sed -i 's#=ZS_HistoryTextCacheSize#='${ZS_HistoryTextCacheSize}'#g' /usr/local/etc/zabbix_server.conf
+  sed -i 's#=ZS_ValueCacheSize#='${ZS_ValueCacheSize}'#g' /usr/local/etc/zabbix_server.conf
+  sed -i 's#=ZS_Timeout#='${ZS_Timeout}'#g' /usr/local/etc/zabbix_server.conf
+  sed -i 's#=ZS_TrapperTimeout#='${ZS_TrapperTimeout}'#g' /usr/local/etc/zabbix_server.conf
+  sed -i 's#=ZS_UnreachablePeriod#='${ZS_UnreachablePeriod}'#g' /usr/local/etc/zabbix_server.conf
+  sed -i 's#=ZS_UnavailableDelay#='${ZS_UnavailableDelay}'#g' /usr/local/etc/zabbix_server.conf
+  sed -i 's#=ZS_UnreachableDelay#='${ZS_UnreachableDelay}'#g' /usr/local/etc/zabbix_server.conf
+  sed -i 's#=ZS_AlertScriptsPath#='${ZS_AlertScriptsPath}'#g' /usr/local/etc/zabbix_server.conf
+  sed -i 's#=ZS_ExternalScripts#='${ZS_ExternalScripts}'#g' /usr/local/etc/zabbix_server.conf
+  sed -i 's#=ZS_FpingLocation#='${ZS_FpingLocation}'#g' /usr/local/etc/zabbix_server.conf
+  sed -i 's#=ZS_Fping6Location#='${ZS_Fping6Location}'#g' /usr/local/etc/zabbix_server.conf
+  sed -i 's#=ZS_SSHKeyLocation#='${ZS_SSHKeyLocation}'#g' /usr/local/etc/zabbix_server.conf
+  sed -i 's#=ZS_LogSlowQueries#='${ZS_LogSlowQueries}'#g' /usr/local/etc/zabbix_server.conf
+  sed -i 's#=ZS_TmpDir#='${ZS_TmpDir}'#g' /usr/local/etc/zabbix_server.conf
+  sed -i 's#=ZS_StartProxyPollers#='${ZS_StartProxyPollers}'#g' /usr/local/etc/zabbix_server.conf
+  sed -i 's#=ZS_ProxyConfigFrequency#='${ZS_ProxyConfigFrequency}'#g' /usr/local/etc/zabbix_server.conf
+  sed -i 's#=ZS_ProxyDataFrequency#='${ZS_ProxyDataFrequency}'#g' /usr/local/etc/zabbix_server.conf
+  sed -i 's#=ZS_AllowRoot#='${ZS_AllowRoot}'#g' /usr/local/etc/zabbix_server.conf
+  sed -i 's#=ZS_User#='${ZS_User}'#g' /usr/local/etc/zabbix_server.conf
+  if [ "$ZS_Include" != "" ]; then
+    echo Include=${ZS_Include} >> /usr/local/etc/zabbix_server.conf
+  fi
+  sed -i 's#=ZS_SSLCertLocation#='${ZS_SSLCertLocation}'#g' /usr/local/etc/zabbix_server.conf
+  sed -i 's#=ZS_SSLKeyLocation#='${ZS_SSLKeyLocation}'#g' /usr/local/etc/zabbix_server.conf
+  sed -i 's#=ZS_SSLCALocation#='${ZS_SSLCALocation}'#g' /usr/local/etc/zabbix_server.conf
+  sed -i 's#=ZS_LoadModulePath#='${ZS_LoadModulePath}'#g' /usr/local/etc/zabbix_server.conf
+  if [ "$ZS_LoadModule" != "" ]; then
+    echo LoadModule=${ZS_LoadModule} >> /usr/local/etc/zabbix_server.conf
+  fi
   sed -i 's/ZS_DBHost/'${ZS_DBHost}'/g' /usr/local/src/zabbix/frontends/php/conf/zabbix.conf.php
   sed -i 's/ZS_DBUser/'${ZS_DBUser}'/g' /usr/local/src/zabbix/frontends/php/conf/zabbix.conf.php
   sed -i 's/ZS_DBPassword/'${ZS_DBPassword}'/g' /usr/local/src/zabbix/frontends/php/conf/zabbix.conf.php
@@ -133,7 +140,7 @@ system_pids
 fix_permissions
 log "Done"
 log "Checking if Database exists or fresh install"
-if ! mysql -u ${DB_USER} -p${DB_PASS} -h ${DB_ADDRESS} -e "use zabbix;"; then
+if ! mysql -u ${ZS_DBUser} -p${ZS_DBPassword} -h ${ZS_DBHost} -P ${ZS_DBPort} -e "use zabbix;"; then
   warning "Zabbix DB doesn't exists. Installing and importing default settings"
   log `create_db`
   log "Database and user created. Importing Default SQL"
@@ -145,3 +152,4 @@ fi
 # TODO wait for zabbix-server start
 #python /config/pyzabbix.py 2>/dev/null
 zabbix_agentd -c /usr/local/etc/zabbix_agentd.conf
+
