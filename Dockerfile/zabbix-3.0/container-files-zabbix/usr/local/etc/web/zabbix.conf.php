0dd3a2792546dfd56b9631c8f3567808f7491ccd
Jan Garaj <jan.garaj@gmail.com>
2016-01-07 23:23:38 +0000

    3.0 healthcheck

--- Dockerfile/zabbix-3.0/container-files-zabbix/usr/local/etc/web/zabbix.conf.php
+++ Dockerfile/zabbix-3.0/container-files-zabbix/usr/local/etc/web/zabbix.conf.php
@@ -12,9 +12,9 @@ $DB['PASSWORD'] = 'ZS_DBPassword';
 // SCHEMA is relevant only for IBM_DB2 database
 $DB['SCHEMA'] = '';
 
-$ZBX_SERVER      = 'localhost';
-$ZBX_SERVER_PORT = '10051';
-$ZBX_SERVER_NAME = 'Zabbix Server';
+$ZBX_SERVER      = 'ZW_ZBX_SERVER';
+$ZBX_SERVER_PORT = 'ZW_ZBX_SERVER_PORT';
+$ZBX_SERVER_NAME = 'ZW_ZBX_SERVER_NAME';
 
 $IMAGE_FORMAT_DEFAULT = IMAGE_FORMAT_PNG;
 ?>
