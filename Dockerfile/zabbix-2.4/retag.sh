0ede71ce6fee8f49156f2e77d02e9117bddca6c4
Jan Garaj <jan.garaj@gmail.com>
2016-01-17 15:02:39 +0000

    rc tags deleted

--- Dockerfile/zabbix-2.4/retag.sh
+++ Dockerfile/zabbix-2.4/retag.sh
@@ -13,7 +13,7 @@ done
 git push origin master
 git push origin --tags
 
-tags=('2.4.0rc1' '2.4.0rc2' '2.4.0rc3' '2.4.1' '2.4.1rc1' '2.4.1rc2' '2.4.2' '2.4.2rc1' '2.4.3' '2.4.3rc1' '2.4.4' '2.4.4rc1' '2.4.5' '2.4.5rc1' '2.4.6' '2.4.7');
+tags=('2.4.0' '2.4.1' '2.4.2' '2.4.3' '2.4.4' '2.4.5' '2.4.6' '2.4.7');
 for t in "${tags[@]}"
 do
     echo "Creating tags $t"
