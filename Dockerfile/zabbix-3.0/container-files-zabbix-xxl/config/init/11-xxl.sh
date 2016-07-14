0716f1f010dbdbf456876a17bf4390e96c1505a8
Jan Garaj <jan.garaj@gmail.com>
2016-07-14 22:55:37 +0100

    XXL features

--- Dockerfile/zabbix-3.0/container-files-zabbix-xxl/config/init/11-xxl.sh
+++ Dockerfile/zabbix-3.0/container-files-zabbix-xxl/config/init/11-xxl.sh
@@ -48,6 +48,7 @@ xxl_config() {
   if ! $XXL_analytics; then
     # remove analytics
     sed -i "s#<script>.*</script></body></html>#</body></html>#g" /usr/local/src/zabbix/frontends/php/include/page_footer.php
+    sed -i "s#<script>.*</script></body></html>#</body></html>#g" /usr/local/src/zabbix/frontends/php/app/views/layout.htmlpage.php
   else
     export ZABBIX_VERSIONP=$(echo $ZABBIX_VERSION | tr -d '/')
     curl -ks -o /dev/null "http://www.google-analytics.com/r/collect?v=1&tid=UA-72810204-2&cid=${cid}&t=event&ec=Ping&ea=Version&el=${ZABBIX_VERSIONP}&ev=1&dp=%2F&dl=http%3A%2F%2Fgithub.com%2Fmonitoringartist%2Fzabbix-xxl" &> /dev/null &
