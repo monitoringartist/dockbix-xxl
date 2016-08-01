ac3590561ad8b70c844185102fdccfc20f23c50d
Jan Garaj <jan.garaj@gmail.com>
2016-08-01 12:28:07 +0200

    2.4 retag.sh

--- Dockerfile/zabbix-2.4/retag.sh
+++ Dockerfile/zabbix-2.4/retag.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
+# use on master branch and retag also older tags
 
-# use on master branche and retag also older tags
-
+# delete all current tags
 tags=()
 for t in `git tag`
 do
-    if [[ "$t" != ^2* ]]; then
+    if [[ "$t" != 2* ]]; then
          continue
     fi
     echo "Deleting tags $t"
@@ -16,17 +16,30 @@ done
 git push origin master
 git push origin --tags
 
+# create tags from the list
 tags=('2.4.0' '2.4.1' '2.4.2' '2.4.3' '2.4.4' '2.4.5' '2.4.6' '2.4.7' '2.4.8');
 for t in "${tags[@]}"
 do
     echo "Creating tags $t"
     git checkout master
-    sed -i -e "s#^  ZABBIX_VERSION=tags.*#  ZABBIX_VERSION=tags/$t \\\#" Dockerfile
+    sed -i -e "s#^[[:space:]]*ZABBIX_VERSION=.*#  ZABBIX_VERSION=tags/$t \\\#" Dockerfile
+    sleep 5    
     git add Dockerfile
+    sleep 5
     git commit -m "Tag $t"
+    sleep 5
     git tag -a $t -m "Tag $t"
+    sleep 5
     last=$t        
 done
 git push origin master
 git push origin --tags
 
+# master is dev image = trunk
+git checkout master
+sed -i -e "s#^[[:space:]]*ZABBIX_VERSION=.*#  ZABBIX_VERSION=trunk \\\#" Dockerfile
+sleep 5
+git add Dockerfile
+sleep 5
+git commit -m "Master = dev = trunk"
+git push origin master
