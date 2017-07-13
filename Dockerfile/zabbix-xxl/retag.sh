d8c5410ffdc9c8e919159f9b1d7b3a0d3ccc178b
Jan Garaj <jan.garaj@gmail.com>
2017-07-13 18:12:10 +0100

    Retag.sh update

--- Dockerfile/zabbix-xxl/retag.sh
+++ Dockerfile/zabbix-xxl/retag.sh
@@ -17,7 +17,7 @@ git push origin master
 git push origin --tags
 
 # create tags from the list
-tags=('3.0.0' '3.0.1' '3.0.2' '3.0.3' '3.0.4' '3.0.5' '3.0.6' '3.0.7' '3.0.8' '3.0.9' '3.2.0' '3.2.1' '3.2.2' '3.2.3' '3.2.4' '3.2.5' '3.2.6');
+tags=('3.0.0' '3.0.1' '3.0.2' '3.0.3' '3.0.4' '3.0.5' '3.0.6' '3.0.7' '3.0.8' '3.0.9' '3.0.10' '3.2.0' '3.2.1' '3.2.2' '3.2.3' '3.2.4' '3.2.5' '3.2.6' '3.2.7');
 for t in "${tags[@]}"
 do
     echo "Creating tags $t"
@@ -35,12 +35,12 @@ done
 git push origin master
 git push origin --tags
 
-# master is dev image = trunk
+# master is the latest stable tag
 git checkout master
-sed -i -e "s#^[[:space:]]*ZABBIX_VERSION=.*#  ZABBIX_VERSION=trunk \\\#" Dockerfile
+sed -i -e "s#^[[:space:]]*ZABBIX_VERSION=.*#  ZABBIX_VERSION=tags/3.2.6 \\\#" Dockerfile
 sleep 5
 git add Dockerfile
 sleep 5
-git commit -m "Master = dev = trunk"
+git commit -m "Master = the latest stable tag"
 git push origin master
 
